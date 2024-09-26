9c3ac56063730d93f1834be48b679d5853ad495c
erickcifu <erickalfred29@gmail.com>
2024-09-25 21:58:26 -0600

    cambios en el app para servidor

--- src/app.ts
+++ src/app.ts
@@ -18,9 +18,11 @@ app.use(authRoutes)
      connectMongoDB();
      console.log('Conexión a la base de datos Mongo establecida');
 
-     app.listen(3000, () => {
-       console.log('Servidor corriendo en el puerto 3000');
+     const port = process.env.PORT || 3000;
+     app.listen(port, () => {
+       console.log(`Server running on port ${port}`);
      });
+     
    })
    .catch((error) => console.log('Error en la conexión a la base de datos:', error));
   
