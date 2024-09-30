0d963f02f01be320d4f6968a441128de182dd6bd
erickcifu <erickalfred29@gmail.com>
2024-09-30 08:35:35 -0600

    un cambio mas

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -6,11 +6,11 @@ import { Tarea } from '../entities/Tarea';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'proyectodb.chqqie6qssu0.us-east-2.rds.amazonaws.com',
+  host: 'testdb.chqqie6qssu0.us-east-2.rds.amazonaws.com',
   port: 3306,
   username: 'root',
   password: 'Amazonbd24.',  
-  database: 'ProyectoBD',
+  database: 'TestDB',
   synchronize: false,  // Para desarrollo, en producción debe ser false
   logging: true,
   entities: [User, Rol, Proyecto, Tarea],  // Importa las entidades
